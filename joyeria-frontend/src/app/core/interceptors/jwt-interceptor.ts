12e44366fe38e3e89bc78da1453df7d725395304
Manu9099 <manucht51@gmail.com>
2026-03-08 17:42:07 -0500

    convert frontend from interceptor

--- joyeria-frontend/src/app/core/interceptors/jwt-interceptor.ts
+++ joyeria-frontend/src/app/core/interceptors/jwt-interceptor.ts
@@ -4,16 +4,28 @@ import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token') || sessionStorage.getItem('token');
   const router = inject(Router);
 
+  const isAuthRequest =
+    req.url.includes('/api/auth/login') ||
+    req.url.includes('/api/auth/registro');
+
+  // No enviar token en login/registro
+  if (isAuthRequest) {
+    return next(req).pipe(
+      catchError(error => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  const token = localStorage.getItem('token') || sessionStorage.getItem('token');
   let clonedReq = req;
 
   if (token) {
     clonedReq = req.clone({
       setHeaders: {
-        'Authorization': `Bearer ${token}`
-        // ✅ Sin Content-Type — el navegador lo pone automático según el tipo de petición
+        Authorization: `Bearer ${token}`
       }
     });
   }
